46e590d8fb712d9c232b2b525d232eba5fe996a0
cortezWarren <cortezwarren6@gmail.com>
2024-09-16 02:27:26 +0800

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, //default route
+  { path: 'home', component: HomePageComponent }
+];
